0b3fcaf9a5225753612f1f14d08c96f5988c937e
chck <deadline.is.today@gmail.com>
2018-12-03 02:42:50 +0900

    Add logger

--- salad/__init__.py
+++ salad/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+from logging import INFO, getLogger, StreamHandler
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(INFO)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+logger.propagate = False

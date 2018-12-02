6cca86e5a6f948b396177b6970c0718260af488a
chck <deadline.is.today@gmail.com>
2018-12-03 04:38:30 +0900

    Modify noisy text for test to avoid orthographic variants

--- tests/salad/preprocessing/text_test.py
+++ tests/salad/preprocessing/text_test.py
@@ -6,10 +6,10 @@ from salad.preprocessing.text import Tokenizer
 
 def test_tokenize():
     noisy_text = '''
-    100年勤めたNTTを退職しました。http://bit.ly/abcde
+    長年勤めたNTTを退職しました。http://bit.ly/abcde
     以上が転職した経緯です。
     '''
-    expected = '100年 勤め た NTT を 退職 し まし た 。 以上 が 転職 し た 経緯 です 。'
+    expected = '長年 勤め た NTT を 退職 し まし た 。 以上 が 転職 し た 経緯 です 。'
     tokenizer = Tokenizer()
     actual = tokenizer.parse(noisy_text)
     assert expected == actual
